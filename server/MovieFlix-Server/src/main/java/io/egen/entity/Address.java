4a2438e5db6f3624298d80646d19c74a33ed95d6
sravanthi Reddy <sravanthi Reddy>
2016-07-29 16:30:13 -0400

    feat(module-api): init entities

---
+++ server/MovieFlix-Server/src/main/java/io/egen/entity/Address.java
@@ -0,0 +1,31 @@
+package io.egen.entity;
+
+import javax.persistence.Embeddable;
+
+@Embeddable
+public class Address {
+
+	private String city;
+	private String street;
+	private String zip;
+	public String getCity() {
+		return city;
+	}
+	public void setCity(String city) {
+		this.city = city;
+	}
+	public String getStreet() {
+		return street;
+	}
+	public void setStreet(String street) {
+		this.street = street;
+	}
+	public String getZip() {
+		return zip;
+	}
+	public void setZip(String zip) {
+		this.zip = zip;
+	}
+	
+	
+}
---
+++ server/MovieFlix-Server/src/main/java/io/egen/entity/Customer.java
@@ -0,0 +1,66 @@
+package io.egen.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Embedded;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
+
+import org.hibernate.annotations.GenericGenerator;
+
+@Entity
+@Table
+@NamedQueries({ 
+	@NamedQuery(name = "Customer.findAll", query = "SELECT c FROM Customer c ORDER BY c.emailId ASC"),
+	@NamedQuery(name = "Customer.findByEmail", query = "SELECT c FROM Customer c WHERE c.emailId=:pEmail") 
+})
+public class Customer {
+
+	@Id
+	@GenericGenerator(name = "customUUID", strategy = "uuid2")
+	@GeneratedValue(generator = "customUUID")
+	private String id;
+	
+	private String lastName;
+	private String firstName;
+	
+	@Column(unique = true)
+	private String emailId;
+	
+	@Embedded
+	private Address address;
+	
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getEmailId() {
+		return emailId;
+	}
+	public void setEmailId(String emailId) {
+		this.emailId = emailId;
+	}
+	public Address getAddress() {
+		return address;
+	}
+	public void setAddress(Address address) {
+		this.address = address;
+	}
+}
