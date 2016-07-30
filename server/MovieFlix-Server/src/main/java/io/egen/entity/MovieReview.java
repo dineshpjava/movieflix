cc81caee6648acbe1566d558d22e4df0b8766590
sravanthi Reddy <sravanthi Reddy>
2016-07-30 01:10:46 -0400

    feat(module-api): init moviereview entity

---
+++ server/MovieFlix-Server/src/main/java/io/egen/entity/MovieReview.java
@@ -0,0 +1,69 @@
+package io.egen.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+import org.hibernate.annotations.GenericGenerator;
+
+@Entity
+@Table
+public class MovieReview {
+
+	@Id
+	@GenericGenerator(name = "customUUID", strategy = "uuid2")
+	@GeneratedValue(generator = "customUUID")
+	private String reviewId;
+	
+	private String comment;
+	
+	private Integer rate;
+	
+	@ManyToOne
+	private Movie movie;
+	
+	@ManyToOne
+	private Customer customer;
+
+	public String getReviewId() {
+		return reviewId;
+	}
+
+	public void setReviewId(String reviewId) {
+		this.reviewId = reviewId;
+	}
+
+	public String getComment() {
+		return comment;
+	}
+
+	public void setComment(String comment) {
+		this.comment = comment;
+	}
+
+	public Integer getRate() {
+		return rate;
+	}
+
+	public void setRate(Integer rate) {
+		this.rate = rate;
+	}
+
+	public Movie getMovie() {
+		return movie;
+	}
+
+	public void setMovie(Movie movie) {
+		this.movie = movie;
+	}
+
+	public Customer getCustomer() {
+		return customer;
+	}
+
+	public void setCustomer(Customer customer) {
+		this.customer = customer;
+	}
+}
